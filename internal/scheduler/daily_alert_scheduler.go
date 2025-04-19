43d30c6d4df02ac5b446785553cc709f5a485f39
Gabriel Souza <gabrielss_@outlook.com>
2025-04-19 02:04:40 +0200

    add timezone to cronjob

--- internal/scheduler/daily_alert_scheduler.go
+++ internal/scheduler/daily_alert_scheduler.go
@@ -2,6 +2,7 @@ package scheduler
 
 import (
 	"log"
+	"time"
 
 	"github.com/gsouza97/my-bots/internal/usecase"
 	"github.com/robfig/cron/v3"
@@ -20,9 +21,14 @@ func NewDailyAlertScheduler(generateDailyAlert *usecase.GenerateDailyAlert) *Dai
 func (s *DailyAlertScheduler) Start() {
 	log.Println("Starting daily alert scheduler")
 
-	c := cron.New(cron.WithSeconds())
+	loc, err := time.LoadLocation("Europe/Madrid")
+	if err != nil {
+		log.Fatalf("failed to load location: %v", err)
+	}
 
-	c.AddFunc("0 */10 * * * *", s.executeDailyAlert)
+	c := cron.New(cron.WithSeconds(), cron.WithLocation(loc))
+
+	c.AddFunc("0 0 8,15,20 * * *", s.executeDailyAlert)
 	c.Start()
 
 	select {}
