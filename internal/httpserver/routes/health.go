a44a6fe849d96256e7e54162b0b8e8d8499f5eac
Gabriel Souza <gabrielss_@outlook.com>
2026-01-20 20:35:27 +0100

    healthcheck for uptime

--- internal/httpserver/routes/health.go
+++ internal/httpserver/routes/health.go
@@ -8,8 +8,13 @@ import (
 
 func StartHealthRoutes(r *gin.Engine) {
 	r.GET("/healthz", handleHealthCheck)
+	r.HEAD("/health", handleUptimeHealthCheck)
 }
 
 func handleHealthCheck(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"Status": "OK"})
+}
+
+func handleUptimeHealthCheck(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"Status": "OK"})
 }
