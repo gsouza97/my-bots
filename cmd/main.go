42401fcf914689e91a61afa358d904916b35af0c
Gabriel Souza <gabrielss_@outlook.com>
2026-01-20 20:22:33 +0100

    Router handle method not allowed

--- cmd/main.go
+++ cmd/main.go
@@ -35,6 +35,7 @@ func main() {
 
 	router := gin.Default()
 	router.Use(cors.Default())
+	router.HandleMethodNotAllowed = true
 	router.Use(cors.New(cors.Config{
 		// AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
 		// AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Accept", "Accept-Encoding", "Accept-Language", "Cache-Control", "Connection", "Referer", "User-Agent"},
